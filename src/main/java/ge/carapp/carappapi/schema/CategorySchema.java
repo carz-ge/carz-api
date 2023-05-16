8fae1339bb05b3e2c1671808bd276e576f0e0006
Liquidibrium <vanoganjelashvili@gmail.com>
2023-05-16 11:29:30 +0400

    Update category

--- src/main/java/ge/carapp/carappapi/schema/CategorySchema.java
+++ src/main/java/ge/carapp/carappapi/schema/CategorySchema.java
@@ -3,11 +3,14 @@ package ge.carapp.carappapi.schema;
 import ge.carapp.carappapi.entity.CategoryEntity;
 import lombok.Builder;
 
+import java.util.UUID;
+
 @Builder
-public record CategorySchema(LingualString name) {
+public record CategorySchema(UUID id, LingualString name) {
 
     public static CategorySchema convert(CategoryEntity categoryEntity) {
         return CategorySchema.builder()
+            .id(categoryEntity.getId())
             .name(new LingualString(categoryEntity.getName(), categoryEntity.getNameKa()))
             .build();
     }
