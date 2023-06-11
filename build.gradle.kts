a0ad8b5c214d058cc0c938a2416e7c254fc745ce
Liquidibrium <vanoganjelashvili@gmail.com>
2023-06-12 01:43:55 +0400

    Add kotlin and cd with github action

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,11 +1,16 @@
+import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
+
 plugins {
     java
-    id("org.springframework.boot") version "3.0.6"
+    id("org.springframework.boot") version "3.1.0"
     id("io.spring.dependency-management") version "1.1.0"
     id("org.graalvm.buildtools.native") version "0.9.20"
     id("org.asciidoctor.jvm.convert") version "3.3.2"
 //    checkstyle
 //    id("com.github.spotbugs") version "5.0.14"
+    id("org.openapi.generator") version "6.6.0"
+    kotlin("jvm") version "1.8.21"
+    kotlin("plugin.spring") version "1.8.21"
 }
 
 group = "ge.carapp"
@@ -68,21 +73,28 @@ dependencies {
     // ULID
     implementation("com.github.f4b6a3:ulid-creator:5.2.0")
 
+    // kotlin
+    implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
+    implementation("io.projectreactor.kotlin:reactor-kotlin-extensions")
+    implementation("org.jetbrains.kotlin:kotlin-reflect")
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-reactor")
+
 //    implementation("org.springframework.cloud:spring-cloud-starter-gateway")
 //    implementation("org.springframework.integration:spring-integration-jdbc")
 //    implementation("org.springframework.integration:spring-integration-jpa")
 //    implementation("org.springframework.integration:spring-integration-mail")
 //    implementation("org.springframework.integration:spring-integration-r2dbc")
 //    implementation("org.springframework.integration:spring-integration-rsocket")
-//    implementation("org.springframework.integration:spring-integration-security")
-//    implementation("org.springframework.integration:spring-integration-stomp")
-//    implementation("org.springframework.integration:spring-integration-webflux")
-//    implementation("org.springframework.integration:spring-integration-websocket")
+    implementation("org.springframework.integration:spring-integration-security")
+    implementation("org.springframework.integration:spring-integration-stomp")
+    implementation("org.springframework.integration:spring-integration-webflux")
+    implementation("org.springframework.integration:spring-integration-websocket")
     implementation("org.springframework.security:spring-security-messaging")
 //    implementation("org.springframework.security:spring-security-rsocket")
 //    implementation("org.springframework.session:spring-session-jdbc")
     compileOnly("org.projectlombok:lombok")
     developmentOnly("org.springframework.boot:spring-boot-devtools")
+    developmentOnly("org.springframework.boot:spring-boot-docker-compose")
 
     // DB drivers
 //    runtimeOnly("mysql:mysql-connector-java:8.0.33")
@@ -105,6 +117,13 @@ dependencies {
 //        mavenBom("org.springframework.cloud:spring-cloud-dependencies:${property("springCloudVersion")}")
 //    }
 //}
+
+tasks.withType<KotlinCompile> {
+    kotlinOptions {
+        freeCompilerArgs = listOf("-Xjsr305=strict")
+        jvmTarget = "17"
+    }
+}
 
 tasks.withType<Test> {
     useJUnitPlatform()
